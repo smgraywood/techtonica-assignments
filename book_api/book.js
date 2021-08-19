b1da4588d53d213fd50b6489e8c1ba7f916bd7eb
Sarah Graywood <smgraywood@gmail.com>
2021-08-18 18:12:34 -0700

    working book form

--- book_api/book.js
+++ book_api/book.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+// Where we will keep books
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
