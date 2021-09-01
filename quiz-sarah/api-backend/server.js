897c20fe35fced4e79ca8a3b5ae0c43eda768a01
Sarah Graywood <smgraywood@gmail.com>
2021-09-01 09:42:09 -0700

    first commit, backend and frontend functioning

---
+++ quiz-sarah/api-backend/server.js
@@ -0,0 +1,84 @@
+//this is the barebones server to test is port is running
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log("Server is running on port 3001!"));
+//this is the barebones server to test is port is running
+
+// const question = JSON.parse(xhttp.responseText);
+
+// const {
+//     id,
+//     title,
+//     choices
+// } = question;
+
+//     document.getElementById('id').value = id;
+//     document.getElementById('title').value = title;
+//     document.getElementById('choices').value = choices;
+   
+
+
+// let cactusQuestions = [
+//     {
+//         id: 1,
+//         title: "A number of Cactus Plants together are called?",
+//         choices: ["Cactuses", "Cacti", "Cactus", "Cact"],
+//         correct: "Cacti"
+//     },
+//     {
+//         id: 2,
+//         title: "Photosynthesis is carried out by cactus via which part of a plant?",
+//         choices: ["Stems", "Leaves", "Roots", "Nodes"],
+//         correct: "Stems",
+//     },
+//     {
+//         id: 3,
+//         title: "Which process does the cactus does only during the night?",
+//         choices: ["Crassulacean acid metabolism", "Photosynthesis", " Growth", "Decomposition"],
+//         correct: "Crassulacean acid metabolism"
+//     },
+//     {
+//         id: 4,
+//         title: "What is the origin of the word «Cactus»?",
+//         choices: ["Greek", "British", " French", "Spanish"],
+//         correct: "Greek"
+//     },
+//     {
+//         id: 5,
+//         title: 'Which group does Cactus belongs to?',
+//         choices: ["Buxbaumiales", "Caryophyllales", "Pteridophytes", "Cactiophytes"],
+//         correct: "Caryophyllales"
+//     },
+//     {
+//         id: 6,
+//         title: "When did Cactus originate?",
+//         choices: ["Cretaceous", "Silurian", "Paleogene", "Modern"],
+//         correct: "Paleogene"
+//     },
+//     {
+//         id: 7,
+//         title: "The smallest Cactus is as small as how many centimeters in diameter?",
+//         choices: ["10", "5", "1", "13"],
+//         correct: "1",
+//     },
+//     {
+//         id: 8,
+//         title: "What can tell us about the age of a cactus?",
+//         choices: ["Height", "Roots", "Spines", "Protrusions"],
+//         correct: "Spines"
+//     },
+//     {
+//         id: 9,
+//         title: "What is the scientific name of Krainz’ Crown Cactus?",
+//         choices: ["Saguaro", "Cactaceae", "Rebutia marsoneri Werderm", "Euphorbia"],
+//         correct: "Rebutia marsoneri Werderm"
+//     },
+//     {
+//         id: 10,
+//         title: "'Saguaros' average life is estimated at how many years?",
+//         choices: ["150-200 years", "80-90 years", "100-150 years", "50-75 years"],
+//         correct: "150-200 years"
+//     }
+//     ]
+    //quiz questions sourced from https://kupidonia.com/quiz-answers/quiz-cactus
